553bb030a0fa22dabf25bb272b1c29f9472dabd8
Rajdeep Mukherjee <rajdeepmukhrj@gmail.com>
2017-04-25 15:00:55 +0530

    added codecs and shebang for encoding

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from Querying import PrepareData, Querying, ComputeResults
 from returnAllMovies import MovieDataSet
 from returnAllUsers import UserDataSet
